a6dc3e3abdd03eacc2a38981f2ea2b5fccd19ee9
thor <thor@otosense.com>
2019-04-24 07:00:18 -0700

    updated README and mint_of_callable.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='py2mint',
+      version='0.0.1',
+      description='Creating, manipulating, and applying mint (Meta-INTerface)',
+      url='https://github.com/i2mint/py2mint',
+      author='Thor Whalen',
+      license='Apache',
+      packages=find_packages())
