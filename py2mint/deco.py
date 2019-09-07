2652728e264ba6cb8981a3d5f57eba46dec780be
twhalen <thor@otosense.com>
2019-09-06 19:08:54 -0700

    add_method (decorator to add methods to objects)

--- py2mint/deco.py
+++ py2mint/deco.py
@@ -1,6 +1,7 @@
 from functools import wraps
 import inspect
 from collections import defaultdict
+from itertools import chain
 
 
 def mk_args_kwargs_merger(func):
@@ -622,7 +623,63 @@ def wrap_class_methods_input_and_output(_return_a_copy_of_the_class=True,
     # return class_wrapper
 
 
-from ocore.utils.pobj import add_method
+def add_method(obj, method_func, method_name=None, class_name=None):
+    """
+    Dynamically add a method to an object.
+
+    :param obj: The object to add a method to
+    :param method_func: The function to use as a method. The first argument must be the object itself
+        (usually called self)
+    :param method_name: The desired function name. If None, will take method_func.__name__
+    :param class_name:  The desired class name. If None, will take type(obj).__name__
+    :return: the object, but with the additional method (or a different function for it)
+
+    >>> class A:
+    ...     def __init__(self, x=10):
+    ...         self.x = x
+    >>> def times(self, y):
+    ...     return self.x * y
+    >>> def plus(self, y):
+    ...     return self.x + y
+    >>> a = A(x=10)
+    >>> a = add_method(a, plus, '__call__')  # add a __call__ method, assigning it to plus
+    >>> a(2)
+    12
+    >>> a = add_method(a, times, '__call__')  # reassign the __call__ method to times instead
+    >>> a(2)
+    20
+    >>> a = add_method(a, plus, '__getitem__')  # assign the method __getitem__ to plus
+    >>> a[2]  # see that it works
+    12
+    >>> a(2)  # and that we still have our __call__ method
+    20
+    """
+    if isinstance(method_func, str):
+        method_name = method_func
+        method_func = getattr(obj, method_name)
+    if method_name is None:
+        method_name = method_func.__name__
+
+    base = type(obj)
+
+    if class_name is None:
+        class_name = base.__name__
+    bases = (base.__bases__[1:]) + (base,)
+    bases_names = set(map(lambda x: x.__name__, bases))
+    if class_name in bases_names:
+        for i in range(6):
+            class_name += '_'
+            if not class_name in bases_names:
+                break
+        else:
+            raise ValueError("can't find a name for class that is not taken by bases. Consider using explicit name")
+
+    new_keys = set(dir(obj)) - set(chain(*[dir(b) for b in bases]))
+
+    d = {a: getattr(obj, a) for a in new_keys}
+    d[method_name] = method_func
+
+    return type(class_name, bases, d)()
 
 
 def transform_instance_method_input_and_output(obj, method, method_output_trans=None, **arg_trans):
@@ -721,7 +778,7 @@ def _call_signature(func: Callable, args: Args, kwargs: Kwargs) -> str:
     """
     args_signature = ", ".join(map(_special_str, args))
     kwargs_signature = ", ".join(("{}={}".format(k, _special_str(v)) for k, v in kwargs.items()))
-    return "Executing: {func_name}({signature})".format(func_name=func.__name__,
+    return "{func_name}({signature})".format(func_name=func.__name__,
                                                         signature=", ".join([args_signature, kwargs_signature]))
 
 
