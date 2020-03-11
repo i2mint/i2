d84c0d92fbc63f6f61f7999b4e6eb38be7c6aeef
twhalen <thor@otosense.com>
2020-03-10 17:15:35 -0700

    new_type

--- py2mint/itypes.py
+++ py2mint/itypes.py
@@ -1,11 +1,35 @@
 from typing import NewType, Optional, Iterable
 
 
-def new_type(name, tp, doc: Optional[str] = None, aka: Optional[Iterable] = None):
-    """Make a new type with (optional) doc and (optional) aka, set of var names it often appears as"""
+def new_type(name, tp, doc: Optional[str] = None, aka: Optional[Iterable] = None, assign_to_globals=False):
+    """
+    Make a new type with (optional) doc and (optional) aka, set of var names it often appears as
+
+    Args:
+        name: Name to give the variable
+        tp: type (see typing module)
+        doc: Optional string to put in __doc__ attribute
+        aka: Optional set (or any iterable) to put in _aka attribute,
+            meant to list names the variables of this type often appear as.
+
+    Returns: None
+
+    >>> from typing import Any, Union, List
+    >>> MyType = new_type('MyType', int)
+    >>> type(MyType)
+    <class 'function'>
+    >>> Key = new_type('Key', Any, aka=['key', 'k'])
+    >>> Key._aka
+    {'key', 'k'}
+    >>> Val = new_type('Val', Union[int, float, List[Union[int, float]]], doc="A number or list of numbers.")
+    >>> Val.__doc__
+    'A number or list of numbers.'
+    """
     new_tp = NewType(name, tp)
     if doc is not None:
         setattr(new_tp, '__doc__', doc)
     if aka is not None:
         setattr(new_tp, '_aka', set(aka))
-    globals()[name] = new_tp
+    if assign_to_globals:
+        globals()[name] = new_tp  # not sure how kosher this is... Should only use at top level of module, for sure!
+    return new_tp
