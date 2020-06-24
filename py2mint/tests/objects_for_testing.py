cd6f11ba8c8fd50ac6f5c5993e913a49dbda2ee8
twhalen <thor@otosense.com>
2020-06-24 14:22:00 -0700

    objects_for_testing.py

--- py2mint/tests/objects_for_testing.py
+++ py2mint/tests/objects_for_testing.py
@@ -1,3 +1,24 @@
+"""
+A module containing a bunch of objects for testing.
+
+See also: https://github.com/i2mint/py2http/blob/master/py2http/tests/objects_for_testing.py
+"""
+
+
+def add(a, b: float = 0.0) -> float:
+    """Adds numbers"""
+    return a + b
+
+
+def mult(x: float, y=1):
+    return x * y
+
+
+# This one has every of the 4 combinations of (default y/n, annotated y/n)
+def formula1(w, x: float, y=1, z: int = 1):
+    return ((w + x) * y) ** z
+
+
 def test_func_1(any_var,
                 a_list: list,
                 a_dict: dict,
