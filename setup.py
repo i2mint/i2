d3a0160359a9a9be37709ad13d82e144a7456ecb
Thor Whalen <thorwhalen1@gmail.com>
2020-07-15 14:49:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='py2mint',
+setup(name='i2',
       version='0.0.1',
       description='Creating, manipulating, and applying mint (Meta-INTerface)',
       url='https://github.com/i2mint/py2mint',
-      author='Thor Whalen',
-      license='Apache',
+      author='OtoSense',
+      license='MIT',
       packages=find_packages())
