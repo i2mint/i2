8c24143e876b8d90d3f9fecb3b93623aef7a21a5
twhalen <thor@otosense.com>
2020-09-23 12:50:46 -0700

    new deploy/setup modules

--- setup.py
+++ setup.py
@@ -1,35 +1,58 @@
-from setuptools import setup, find_packages
+import os
+from configparser import ConfigParser
+from setuptools import find_packages
+from pack import read_configs
 
-p = dict(version="0.0.4",
-         root_url='https://github.com/i2mint',
-         name='i2'
-         )
 
-extras = dict(
-    description='Creating, manipulating, and applying mint (Meta-INTerface)',
-    author='OtoSense',
-    license='Apache',
-    keywords=['i2i', 'meta'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        # Either
-        # "3 - Alpha",
-        # "4 - Beta" or
-        # "5 - Production/Stable" as the current state.
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.7',
-    ],
-    # to get install_requires you can use imports_for.third_party (py2store.ext.module_imports)
-    install_requires=[
-    ]
+def my_setup(print_params=True, **setup_kwargs):
+    from setuptools import setup
+    if print_params:
+        import json
+        print("Setup params -------------------------------------------------------")
+        print(json.dumps(setup_kwargs, indent=2))
+        print("--------------------------------------------------------------------")
+    setup(**setup_kwargs)
+
+
+# read the config file (get a dict with it's contents)
+root_dir = os.path.dirname(__file__)
+config_file = os.path.join(root_dir, 'setup.cfg')
+configs = read_configs(config_file, section='metadata')
+
+# parse out name and root_url
+name = configs['name']
+root_url = configs['root_url']
+version = configs.get('version', None)
+
+# Note: if version is not in config, version will be None,
+#  resulting in bumping the version or making it be 0.0.1 if the package is not found (i.e. first deploy)
+
+meta_data_dict = {k: v for k, v in configs.items()}
+
+# make the setup_kwargs
+setup_kwargs = dict(
+    meta_data_dict,
+    # You can add more key=val pairs here if they're missing in config file
 )
 
-assert {'root_url', 'name', 'version'}.issubset(p.keys())
+# import os
+# name = os.path.split(os.path.dirname(__file__))[-1]
+
+if version is None:
+    try:
+        from pack import next_version_for_package
+
+        version = next_version_for_package(name)  # when you want to make a new package
+    except Exception as e:
+        print(f"Got an error trying to get the new version of {name} so will try to get the version from setup.cfg...")
+        print(f"{e}")
+        version = configs.get('version', None)
+        if version is None:
+            raise ValueError(f"Couldn't fetch the next version from PyPi (no API token?), "
+                             f"nor did I find a version in setup.cfg (metadata section).")
 
 
-def readme():
+def text_of_readme_md_file():
     try:
         with open('README.md') as f:
             return f.read()
@@ -39,20 +62,28 @@ def readme():
 
 ujoin = lambda *args: '/'.join(args)
 
-assert not p['root_url'].endswith('/'), f"root_url should not end with /: {p['root_url']}"
+if root_url.endswith('/'):
+    root_url = root_url[:-1]
 
-attrs = dict(name=p['name'],
-             version=f"{p['version']}",
-             long_description=readme(),
-             long_description_content_type="text/markdown",
-             url=f"{p['root_url']}/{p['name']}",
-             packages=find_packages(),
-             include_package_data=True,
-             zip_safe=False,
-             download_url=f"{p['root_url']}/{p['name']}/archive/v{p['version']}.zip",
-             )
+dflt_kwargs = dict(
+    name=f"{name}",
+    version=f'{version}',
+    url=f"{root_url}/{name}",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    long_description=text_of_readme_md_file(),
+    long_description_content_type="text/markdown",
+)
 
-for k in extras:
-    attrs[k] = extras[k]
+setup_kwargs = dict(dflt_kwargs, **setup_kwargs)
 
-setup(**attrs)
+##########################################################################################
+# Diagnose setup_kwargs
+_, containing_folder_name = os.path.split(os.path.dirname(__file__))
+if setup_kwargs['name'] != containing_folder_name:
+    print(f"!!!! containing_folder_name={containing_folder_name} but setup name is {setup_kwargs['name']}")
+
+##########################################################################################
+# Okay... set it up alright!
+my_setup(**setup_kwargs)
