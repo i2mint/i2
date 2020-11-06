dbbeb26aa0fc56573664acb6eb3b12d0b67cb824
Thor Whalen <thorwhalen1@gmail.com>
2020-11-05 17:00:47 -0800

    non_doctest_lines

--- i2/doc_mint.py
+++ i2/doc_mint.py
@@ -34,8 +34,50 @@ def _prefix_lines(s: str, prefix: str = '# ', even_if_empty: bool = False) -> st
 import doctest
 from typing import Callable
 import re
+from inspect import getdoc
 
 comment_strip_p = re.compile(r'(?m)^ *#.*\n?')
+
+doctest_line_p = re.compile('\s*>>>')
+empty_line = re.compile('\s*$')
+
+
+def non_doctest_lines(doc):
+    r"""Generator of lines of the doc string that are not in a doctest scope.
+
+    >>> def _test_func():
+    ...     '''Line 1
+    ...     Another
+    ...     >>> doctest_1
+    ...     >>> doctest_2
+    ...     line_after_a_doc_test
+    ...     another_line_that_is_in_the_doc_test scope
+    ...
+    ...     But now we're out of a doctest's scope
+    ...
+    ...     >>> Oh no, another doctest!
+    ...     '''
+    >>> from inspect import getdoc
+    >>>
+    >>> list(non_doctest_lines(getdoc(_test_func)))
+    ['Line 1', 'Another', "But now we're out of a doctest's scope", '']
+
+    :param doc:
+    :return:
+    """
+    last_line_was_a_doc_test = False
+    for line in doc.splitlines():
+        if not doctest_line_p.match(line):
+            if not last_line_was_a_doc_test:
+                yield line
+                last_line_was_a_doc_test = False
+            else:
+                if empty_line.match(line):
+                    last_line_was_a_doc_test = False
+        else:
+            last_line_was_a_doc_test = True
+
+
 
 
 def strip_comments(code):
@@ -86,7 +128,7 @@ def _assert_wants(source, want, wrap_func_name=None):
             return f"actual = {source}\nexpected = {want}\nassert actual == expected"
         else:
             return f"actual = {wrap_func_name}({source})\nexpected = {wrap_func_name}({want})\n" \
-                "assert actual == expected"
+                   "assert actual == expected"
 
 
 def _output_prefix(source, want, prefix='# OUTPUT: '):
