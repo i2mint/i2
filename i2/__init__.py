5697b86b09f17e5a70a2d0e6f2450127f633c8a1
Thor Whalen <thorwhalen1@gmail.com>
2020-12-23 16:44:16 -0800

    changed setup (cfg and py)

--- i2/__init__.py
+++ i2/__init__.py
@@ -1,1 +1,8 @@
-
+from i2.deco import (
+    preprocess,
+    postprocess,
+    preprocess_arguments,
+    input_output_decorator,
+)
+from i2.deco import wrap_class_methods_input_and_output
+from i2.signatures import Sig
