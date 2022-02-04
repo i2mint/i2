71abc08d3ec9c4d27b07e1b26ae245a5e04405c9
GitHub CI Runner <thorwhalen1@gmail.com>
2022-02-04 01:43:26 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- i2/wrapper.py
+++ i2/wrapper.py
@@ -227,7 +227,7 @@ class Wrap:
             # ... but an ingress factory: Should make the ingress in function of func
             self.ingress = func_to_ingress(func)
         else:
-            assert callable(ingress), f"Should be callable: {ingress}"
+            assert callable(ingress), f'Should be callable: {ingress}'
             self.ingress = ingress
 
         return_annotation = empty
@@ -575,7 +575,7 @@ def invert_map(d: dict):
     if len(new_d) == len(d):
         return new_d
     else:
-        raise ValueError(f"There are duplicate keys so I can invert map: {d}")
+        raise ValueError(f'There are duplicate keys so I can invert map: {d}')
 
 
 from i2.signatures import parameter_to_dict
@@ -739,9 +739,9 @@ class InnerMapIngress:
         self.kwargs_trans = kwargs_trans
 
         outer_name_for_inner_name = {
-            inner_name: change["name"]
+            inner_name: change['name']
             for inner_name, change in changes_for_name.items()
-            if "name" in change
+            if 'name' in change
         }
         self.inner_name_for_outer_name = invert_map(outer_name_for_inner_name)
         self.outer_sig(self)
@@ -913,8 +913,8 @@ def arg_val_converter_ingress(func, __strict=True, **conversion_for_arg):
     if __strict:
         conversion_names_that_are_not_func_args = conversion_for_arg.keys() - sig.names
         assert not conversion_names_that_are_not_func_args, (
-            "Some of the arguments you want to convert are not argument names "
-            f"for the function: {conversion_names_that_are_not_func_args}"
+            'Some of the arguments you want to convert are not argument names '
+            f'for the function: {conversion_names_that_are_not_func_args}'
         )
 
     @sig
@@ -937,8 +937,8 @@ class ArgValConverterIngress:
                 conversion_for_arg.keys() - sig.names
             )
             assert not conversion_names_that_are_not_func_args, (
-                "Some of the arguments you want to convert are not argument names "
-                f"for the function: {conversion_names_that_are_not_func_args}"
+                'Some of the arguments you want to convert are not argument names '
+                f'for the function: {conversion_names_that_are_not_func_args}'
             )
         self.sig = sig
         self.conversion_for_arg = conversion_for_arg
@@ -979,11 +979,11 @@ def camelize(s):
     >>> camelize('camel_case')
     'CamelCase'
     """
-    return "".join(ele.title() for ele in s.split("_"))
+    return ''.join(ele.title() for ele in s.split('_'))
 
 
 def kwargs_trans_to_extract_args_from_attrs(
-    outer_kwargs: dict, attr_names=(), obj_param="self"
+    outer_kwargs: dict, attr_names=(), obj_param='self'
 ):
     self = outer_kwargs.pop(obj_param)
     arguments_extracted_from_obj = {name: getattr(self, name) for name in attr_names}
@@ -1001,7 +1001,7 @@ def param_to_dataclass_field_tuple(param: Parameter):
         if len(t) == 2:
             t = t[0]
         else:
-            t = (t[0], "typing.Any", t[2])
+            t = (t[0], 'typing.Any', t[2])
     return t
 
 
@@ -1014,7 +1014,7 @@ def func_to_method_func(
     *,
     method_name=None,
     method_params=None,
-    instance_arg_name="self",
+    instance_arg_name='self',
 ) -> MethodFunc:
     """Get a 'method function' from a 'normal function'.
 
@@ -1099,9 +1099,7 @@ from typing import Iterable
 
 
 def make_funcs_binding_class(
-    funcs,
-    init_params=(),
-    cls_name=None,
+    funcs, init_params=(), cls_name=None,
 ):
     """Transform one or several functions into a class that contains them as methods
     sourcing specific arguments from the instance's attributes.
@@ -1131,10 +1129,10 @@ def make_funcs_binding_class(
     'goodbye: 33'
     """
 
-    dflt_cls_name = "FuncsUnion"
+    dflt_cls_name = 'FuncsUnion'
     if callable(funcs) and not isinstance(funcs, Iterable):
         single_func = funcs
-        dflt_cls_name = camelize(getattr(single_func, "__name__", dflt_cls_name))
+        dflt_cls_name = camelize(getattr(single_func, '__name__', dflt_cls_name))
         funcs = [single_func]
 
     cls_name = cls_name or dflt_cls_name
