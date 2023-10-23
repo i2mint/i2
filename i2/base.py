d280b06ac6e771481aed7b26c56b620b7663e046
Thor Whalen <thorwhalen1@gmail.com>
2023-10-23 17:06:53 +0200

    feat: deprecate base.name_of_obj

--- i2/base.py
+++ i2/base.py
@@ -47,39 +47,20 @@ def _property_names_of(obj):
             yield attr_name
 
 
-# Note: For a more powerful, and customizable implementation, 
+# Note: For a more powerful, and customizable implementation,
 #  see `i2.signatures.name_of_obj`.
 # TODO: Analyse usages and reroute to i2.signatures.name_of_obj instead.
 def name_of_obj(o):
-    """
-    Tries to find the (or "a") name for an object, even if `__name__` doesn't exist.
+    from warnings import warn
 
-    This is a basic implementation, and it's not guaranteed to work for all objects.
-    For a more powerful, and customizable implementation, 
-    see `i2.signatures.name_of_obj`.
+    warn(
+        "Deprecated: Use i2.name_of_obj or i2.signatures.name_of_object instead",
+        DeprecationWarning,
+    )
 
-    >>> name_of_obj(map)
-    'map'
-    >>> name_of_obj([1, 2, 3])
-    'list'
-    >>> name_of_obj(print)
-    'print'
-    >>> name_of_obj(lambda x: x)
-    '<lambda>'
-    >>> from functools import partial
-    >>> name_of_obj(partial(print, sep=','))
-    'print'
-    """
-    if hasattr(o, '__name__'):
-        return o.__name__
-    elif hasattr(o, '__class__'):
-        name = name_of_obj(o.__class__)
-        if name == 'partial':
-            if hasattr(o, 'func'):
-                return name_of_obj(o.func)
-        return name
-    else:
-        return no_name
+    from i2.signatures import name_of_obj as signatures_name_of_obj
+
+    return signatures_name_of_obj(o)
 
 
 class AttrFromKey:
